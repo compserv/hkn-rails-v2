ccf34f0eee34c31e5e387becf0911d914ae42fc8
Mark Miyashita <negativetwelve@gmail.com>
2013-12-14 17:29:26 -0800

    Initial commit with vanilla Rails 4.0 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HknRails::Application.config.secret_key_base = '4407991c3c0979b14522d09e8567b25e99f216e81d03b161f055dbaa7a28525bec536ee81c27979cfac1f8b4414f523ccb7e2fd32c6aef58e2f0d9ddf1ae8a08'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HknRails::Application.config.session_store :cookie_store, key: '_hkn-rails_session'
