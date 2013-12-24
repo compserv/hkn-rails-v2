427f0373f7e33f000a5d8d5fae1fe4d875fa29b8
Mark Miyashita <negativetwelve@gmail.com>
2013-12-24 02:02:45 -0800

    Bumping schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20131223204009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "events", force: true do |t|
     t.string   "title"
