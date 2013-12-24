4268b16af1b2a27700b24ef088965bd0fe1f8018
Mark Miyashita <negativetwelve@gmail.com>
2013-12-24 14:18:26 -0800

    Adding devise for users back to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 HknRails::Application.routes.draw do
   resources :challenges, only: [:create, :update, :index]
 
+  devise_for :users
+
   get 'candidate/portal' => 'candidate#portal', as: 'candidate_portal'
   get 'candidate/autocomplete_officer_name' => 'candidate#autocomplete_officer_name', as: 'autocomplete_officer_name'
 
