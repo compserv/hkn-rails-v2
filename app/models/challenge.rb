a5d3729349e33f8d98d6028378f2f382da28587c
Kevin Casey <kacasey@berkeley.edu>
2013-12-24 02:28:25 -0800

    minor fixes

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id                     :integer       not null, primary key
+#  requester_id           :integer       not null
+#  candidate_id           :integer       not null
+#  confirmed              :boolean
+#  rejected               :boolean
+#  name                   :string        not null
+#  description            :text
+#  created_at             :datetime
+#  updated_at             :datetime
+#
+
 class Challenge < ActiveRecord::Base
   validates :name, presence: true
+  validates :requester_id, presence: true
+  validates :candidate_id, presence: true
 end
