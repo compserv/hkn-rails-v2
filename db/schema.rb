d9366961e9192fc7ee670f409c37713ba5c51ebf
Mark Miyashita <negativetwelve@gmail.com>
2013-12-24 14:50:37 -0800

    Added member semester to the schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131223204009) do
+ActiveRecord::Schema.define(version: 20131224210759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20131223204009) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "owner_id"
+  end
+
+  create_table "member_semesters", force: true do |t|
+    t.integer  "year"
+    t.string   "season"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "roles", force: true do |t|
