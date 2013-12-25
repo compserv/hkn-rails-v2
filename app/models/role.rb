32607734705b11516b4e7349d90205c2fc59460e
Mark Miyashita <negativetwelve@gmail.com>
2013-12-25 01:39:41 -0800

    Adding extra class methods for finding current officers

--- app/models/role.rb
+++ app/models/role.rb
@@ -15,4 +15,23 @@ class Role < ActiveRecord::Base
   belongs_to :resource, :polymorphic => true
 
   scopify
+
+  class << self
+    def current(position)
+      find_by_name_and_resource_id(position, MemberSemester.current.id)
+    end
+
+    def position_for_semester(position, semester)
+      find_by_name_and_resource_id(position, semester.id)
+    end
+  end
+
+  def semester
+    resource
+  end
+
+  def is_officer?
+    role_type == "officer"
+  end
+
 end
