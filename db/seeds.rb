8599b6cf29f9409f4bbbe3fbf5efb155e49e814a
Mark Miyashita <negativetwelve@gmail.com>
2013-12-24 21:59:58 -0800

    Adding member_semesters to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
+# Member Semesters
+member_semesters = [
+  ["Spring", 2013],
+  ["Fall", 2013],
+  ["Spring", 2014],
+]
+
+member_semesters.each do |member_semester|
+  new_member_semester = MemberSemester.create(season: member_semester[0], year: member_semester[1])
+  puts "Created new member semester: #{new_member_semester.name}."
+end
+
+current_member_semester = MemberSemester.last
+
 # Users
 users = [
   ["kacasey", "kacasey@berkeley.edu"],
@@ -7,7 +21,8 @@ users = [
 
 users.each do |user_info|
   user = User.create(username: user_info[0], email: user_info[1], password: "password", password_confirmation: "password")
-  puts "Created user #{user.id} with username: #{user.username} and email: #{user.email}."
+  user.member_semesters << current_member_semester
+  puts "Created user with username: #{user.username} and email: #{user.email}."
 end
 
 # Roles
@@ -31,4 +46,3 @@ officer_roles.each do |role|
   new_role = Role.create(name: role, resource_type: "officer")
   puts "Created new role: #{new_role.name}."
 end
-
