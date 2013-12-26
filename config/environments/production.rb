f57313581b7a94fd6df54ca6bc87ad03c52cae62
Kevin Casey <kacasey@berkeley.edu>
2013-12-26 12:15:59 -0800

    fixing environments for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,9 @@ HknRails::Application.configure do
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
+  # HACK for hkn-rails on hkn.eecs: with tls, it complains about hostname
+  config.action_mailer.smtp_settings = {:enable_starttls_auto => false}
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
