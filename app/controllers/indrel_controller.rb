8b115eb07ca1211c36647930aaa3c6d259b52ae4
Kevin Casey <kacasey@berkeley.edu>
2013-12-30 20:55:55 -0800

    papercliped user pictures

--- app/controllers/indrel_controller.rb
+++ app/controllers/indrel_controller.rb
@@ -1,5 +1,6 @@
 class IndrelController < ApplicationController
   def contact_us
     @indrel_officers = MemberSemester.current.users.select { |member| member.has_role? :indrel }
+    #@indrel_officers = User.all
   end
 end
