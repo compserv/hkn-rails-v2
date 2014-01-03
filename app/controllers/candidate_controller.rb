9926842cef61789451a2470a2d50502c47ff62f1
abstinenceee <alanyao@berkeley.edu>
2014-01-02 20:05:45 -0800

    Added quiz page

--- app/controllers/candidate_controller.rb
+++ app/controllers/candidate_controller.rb
@@ -5,6 +5,31 @@ class CandidateController < ApplicationController
   end
 
   def quiz
+    quiz_resp = @current_user.candidate_quiz.quiz_responses
+    @quiz_resp = Hash.new('')
+    if !quiz_resp.empty? #Fill hash with default blanks
+      for resp in quiz_resp
+        @quiz_resp[('q' << resp.id.to_s).to_sym] = resp.response
+      end
+    end
+  end
+
+  def submit_quiz
+    params.each do |key, value|
+      if key.match(/^q/) #Starts with "q", is a quiz response
+        quiz_responses = @current_user.candidate.quiz_responses
+        old_answer = quiz_responses.select { |resp| ('q' << resp.quiz_question_id.to_s).to_sym == key }.first
+        if old_answer:
+          old_answer.response = value.to_s
+          old_answer.save
+        else 
+          quiz_responses.create(quiz_question_id: ('q' << resp.quiz_question_id.to_s).to_sym,
+                                response: value.to_s)
+        end
+      end
+    end
+    flash[:notice] = "Your quiz responses have been recorded."
+    redirect_to :back
   end
 
   def autocomplete_officer_name
