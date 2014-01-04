0321c103832359740c0e20e99085bd98e925a2e4
Mark Miyashita <negativetwelve@gmail.com>
2014-01-04 03:28:42 -0700

    Mailer for new infosession requests

---
+++ app/mailers/indrel_mailer.rb
@@ -0,0 +1,11 @@
+class IndrelMailer < ActionMailer::Base
+
+  def infosession_registration(infosession_request)
+    @infosession_request = infosession_request
+    mail
+      to: "indrel@hkn.eecs.berkeley.edu",
+      from: "infosessions-registration@hkn.eecs.berkeley.edu",
+      subject: "Infosession registration from #{@infosession_request.company_name}"
+  end
+
+end
