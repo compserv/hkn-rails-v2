ae4d93c58cd404e8d0200564ffef291f70f54554
Kevin Casey <kacasey@berkeley.edu>
2014-01-12 23:04:37 -0800

    added created_at information to the urls

--- app/controllers/resume_books_controller.rb
+++ app/controllers/resume_books_controller.rb
@@ -195,12 +195,13 @@ class ResumeBooksController < ApplicationController
         end
       end
       if !authorized
+        # maybe email someone that a hack attempt went off? tbh there are 62^100 possibilities and they need to match up the id of the resume book with an active string, it will be absurdly difficult to hack
         render json: "Oops, your link does not appear in our database.  Please email indrel@hkn.eecs.berkeley.edu if this is a mistake!" and return
       end
     else
       authenticate_indrel!
     end
-    send_file @resume_book.pdf.path, type: @resume_book.pdf_content_type, filename: @resume_book.pdf_file_name, disposition: 'inline'
+    send_file @resume_book.pdf.path, type: @resume_book.pdf_content_type, filename: @resume_book.pdf_file_name
   end
 
   def download_iso
