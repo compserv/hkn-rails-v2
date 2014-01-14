d87dc2fa7e82a0379c62f39101e75da20ec97369
Kevin Casey <kacasey@berkeley.edu>
2014-01-13 22:55:31 -0800

    announcements intact

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140113051853) do
+ActiveRecord::Schema.define(version: 20140114053024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20140113051853) do
   end
 
   add_index "alumni", ["user_id"], name: "index_alumni_on_user_id", using: :btree
+
+  create_table "announcements", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "candidate_quizzes", force: true do |t|
     t.integer  "user_id"
