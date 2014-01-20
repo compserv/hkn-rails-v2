cb6e606523628fd2de633fcc0afa9585a5e1812d
brianhou <brian.hou@live.com>
2014-01-20 01:27:41 -0800

    add enrollment numbers from osoc.berkeley.edu

--- script/csec/scrape.rb
+++ script/csec/scrape.rb
@@ -5,8 +5,14 @@
 #  + http://www.eecs.berkeley.edu/Scheduling/EE/schedule.html
 #  + http://www.eecs.berkeley.edu/Scheduling/CS/schedule.html
 #
-# In theory, this could break at any time, but it's probably okay since the dept
-# auto-generates those schedule pages.
+# It also scrapes enrollment data from osoc.berkeley.edu. They aren't real big
+# fans of people doing that, but I think if we limit ourselves to not real-time
+# stats, we might be okay. If the API ever works, we should try to use that.
+#  + http://osoc.berkeley.edu/OSOC/osoc
+#
+# In theory, this could break at any time, but it's probably okay since all
+# those pages are auto-generated. It should take <10 seconds to run; if it takes
+# longer, OSOC probably doesn't like us.
 #  - brian
 
 require 'nokogiri'
@@ -16,6 +22,7 @@ require 'set'
 
 EE_URL = 'http://www.eecs.berkeley.edu/Scheduling/EE/schedule.html'
 CS_URL = 'http://www.eecs.berkeley.edu/Scheduling/CS/schedule.html'
+OSOC_URL = 'http://osoc.berkeley.edu/OSOC/osoc'
 
 # Use Nokogiri to actually scrape the pages
 
@@ -34,16 +41,63 @@ def parse_row(row)
   cells << (!row.search('strong').empty?) # if class is main section
 end
 
+def update_enrollment(courses, term = "SP")
+  # Mutate the Course objects and return a list of courses that are on the
+  # Berkeley schedule, but not on the EECS schedule.
+  ee_enroll = parse_enrollment('Electrical+Engineering', term)
+  cs_enroll = parse_enrollment('Computer+Science', term)
+
+  errors = []
+  (ee_enroll + cs_enroll).each do |course, enrolled|
+    if courses.key?(course)
+      courses[course].enrollment = enrolled
+    else
+      errors << "#{course} (#{enrolled} enrolled)"
+    end
+  end
+  errors
+end
+
+def parse_enrollment(dept, term)
+  # Return array of [:course, enrollment] from all OSOC pages.
+  url = OSOC_URL + "?p_term=#{term}&p_deptname=#{dept}"
+  row = 1
+  pages = []
+  while true
+    puts "Begging #{url} for page #{row/100}"
+    pages << Nokogiri::HTML(open(url + "&p_start_row=#{row}"))
+    first_label = pages.last.search('label')[0].text
+    first_label == 'see next results' ? row += 100 : break
+  end
+  pages.map { |page| parse_osoc(page) }.flatten(1)
+end
+
+def parse_osoc(osoc_page)
+  # Return array of [:course, enrollment] from one OSOC page.
+  result = []
+  osoc_page.search('table')[1..-2].each do |section|
+    cells = section.search('td')
+    name = cells[2].text.split
+    course = "#{name[0][0]}#{name[1][0]}#{name[2]}-#{name[-2].gsub(/^0*/, '')}"
+    units = cells[14].text
+    enrollment = cells[-2].text.split[1].delete('Enrolled:')
+    if !units.empty? # To avoid discussion/lab sections
+      result << [course.downcase.to_sym, enrollment]
+    end
+  end
+  result
+end
+
 # Turn the scraped information into a hash of Course objects
 
 class Course
   attr_reader :name, :section, :prof_list, :time, :place
-  attr_accessor :ta_list
+  attr_accessor :ta_list, :enrollment
 
   def initialize(row)
     @ccn, @name, @section, _, @title, @prof_list, @time, @place = *row
     @ta_list = Set.new
-    @enrollment = '-' # TODO: scrape this from schedule
+    @enrollment = 'UNKNOWN'
 
     # for csv formatting
     @prof_list = @prof_list.split('; ')
@@ -56,8 +110,7 @@ class Course
   end
 
   def to_sym
-    name = @section == 1 ? @name : "#{@name}-#{@section}"
-    name.delete(' ').downcase.to_sym
+    "#{@name}-#{@section}".delete(' ').downcase.to_sym
   end
 
   def to_a
@@ -86,6 +139,12 @@ def make_course(course_rows)
   {course.to_sym => course}
 end
 
+def write_course_info(courses, date)
+  CSV.open("course_info_#{date}.csv", 'wb') do |csv|
+    courses.each { |course| csv << course.to_a }
+  end
+end
+
 def find_overlap(info, block)
   # Return a map sorting elements of info into buckets, according to block.
   # Only keep buckets with more than one element.
@@ -108,23 +167,19 @@ def write_crosslists(courses, date)
   end
 end
 
-def write_course_info(courses, date)
-  CSV.open("course_info_#{date}.csv", 'wb') do |csv|
-    courses.each { |course| csv << course.to_a }
-  end
-end
-
 def main
   ee, cs = parse_schedule(EE_URL), parse_schedule(CS_URL)
   all_classes = ee.drop(1) + cs.drop(1)
   courses = make_courses(all_classes)
+  osoc_errors = update_enrollment(courses)
 
   date = Time.now.strftime('%Y%m%d')
   head = [%w(Course Sec Enrolled Instructor(1) Instructor(2) Time Place TA's)]
   write_course_info(head + courses.values, date)
   write_crosslists(courses.values, date)
-
   puts "Written to course_info_#{date}.csv and crosslists_#{date}.txt."
+
+  puts "The following courses may be missing from the csv output:", osoc_errors
 end
 
 main
