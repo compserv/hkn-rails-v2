1ca9acb5e1fb7e60400ab1b6c77059ca9ee74053
Kevin Casey <kacasey@berkeley.edu>
2014-01-23 18:27:20 -0800

    authentication on new/show, cleaned up _form a little

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,6 +1,6 @@
 class ExamsController < ApplicationController
   before_action :set_exam, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_studrel!, only: [:create, :edit, :update, :destroy]
+  before_filter :authenticate_studrel!, only: [:create, :edit, :update, :destroy, :new, :show]
 
   # GET /exams
   def index
